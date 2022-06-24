973c1ec8868c9d0e0f803c97dbf66a97710341a1
nenad.bjelica <nenad.bjelica@naovis.com>
2022-06-24 12:54:25 +0200

    Allure report done

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
+      allureWriter(on, config)
     },
     baseUrl: "https://www.phptravels.net/login",
     watchForFileChanges: true,
